a5a44d7763f02c5f9197c9bed5c165c2e8df0379
ElijahAmodu <elijahamodu49@gmail.com>
2023-01-05 14:53:28 +0000

    Added js

---
+++ index.js
@@ -0,0 +1,7 @@
+let words = ['hello', 'world', 'this', 'is', 'great'];
+
+function smashWords(value) {
+    let sentence = value.join(' ')
+    return sentence;
+  }
+console.log(smashWords(words));
